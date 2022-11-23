5433368a33442fa450380a82ed1b30cfb21399e6
Alexis Davidson <alexisdavidson00@gmail.com>
2022-11-23 10:37:18 +0800

    Finish unit tests

--- src/backend/contracts/Planting.sol
+++ src/backend/contracts/Planting.sol
@@ -5,47 +5,31 @@ import "@openzeppelin/contracts/utils/Strings.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Planting is Ownable {
-    enum Phase {
-        POT,
-        BEAN,
-        SPROUTING,
-        SAPLING,
-        ADULT
-    }
-
     struct Plant {
-        Phase phase;
+        uint256 phase;
         uint256 timestampPhaseStarted;
     }
 
     mapping(address => Plant) public plantPerUser;
-    mapping(Phase => uint256) public phaseDuration;
+    uint256[5] public phaseDuration;
+    uint256 lastPhase = 4;
 
     event PlantingSuccessful(address user);
 
     constructor() {
-        phaseDuration[Phase.POT] = 0; // Can start planting right away
-        phaseDuration[Phase.BEAN] = 4 * 3600; // 4 Hours
-        phaseDuration[Phase.SPROUTING] = 18 * 3600; // 18 Hours
-        phaseDuration[Phase.SAPLING] = 18 * 3600; // 48 Hours
-        phaseDuration[Phase.ADULT] = 18 * 3600; // 96 Hours
+        phaseDuration[0] = 0; // Can start planting right away
+        phaseDuration[1] = 4 * 3600; // 4 Hours
+        phaseDuration[2] = 18 * 3600; // 18 Hours
+        phaseDuration[3] = 48 * 3600; // 48 Hours
+        phaseDuration[4] = 96 * 3600; // 96 Hours
     }
 
     function plant() public {
         require(currentPhaseFinished(msg.sender), "The current growing phase of your plant is not finished yet!");
-        require(plantPerUser[msg.sender].phase < Phase.ADULT, "Your plant already reached maximum growth!");
+        require(plantPerUser[msg.sender].phase < lastPhase, "Your plant already reached maximum growth!");
 
-        plantPerUser[msg.sender].phase = nextPhase(plantPerUser[msg.sender].phase);
-    }
-
-    function nextPhase(Phase phase) private view returns(Phase) {
-        if (phase == Phase.POT)
-            return Phase.BEAN;
-        if (phase == Phase.BEAN)
-            return Phase.SPROUTING;
-        if (phase == Phase.SPROUTING)
-            return Phase.SAPLING;
-        return Phase.ADULT;
+        plantPerUser[msg.sender].phase += 1;
+        plantPerUser[msg.sender].timestampPhaseStarted = block.timestamp;
     }
 
     function currentPhaseFinished(address _user) public view returns(bool) {
--- src/backend/test/NFT.test.js
+++ src/backend/test/NFT.test.js
@@ -1,9 +1,10 @@
 const { expect } = require("chai")
+const helpers = require("@nomicfoundation/hardhat-network-helpers")
 
 const toWei = (num) => ethers.utils.parseEther(num.toString())
-const fromWei = (num) => ethers.utils.formatEther(num)
+const fromWei = (num) => parseInt(ethers.utils.formatEther(num))
 
-describe("NFT", async function() {
+describe("NFT & Planting", async function() {
     let deployer, addr1, addr2, nft
     let teamWallet = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"
     let whitelist = []
@@ -12,13 +13,15 @@ describe("NFT", async function() {
     beforeEach(async function() {
         // Get contract factories
         const NFT = await ethers.getContractFactory("NFT");
+        const Planting = await ethers.getContractFactory("Planting");
 
         // Get signers
-        [deployer, addr1, addr2] = await ethers.getSigners();
-        whitelist = [addr1.address, addr2.address]
+        [deployer, addr1, addr2, addr3] = await ethers.getSigners();
+        whitelist = [addr1.address, addr2.address, addr3.address]
 
         // Deploy contracts
         nft = await NFT.deploy();
+        planting = await Planting.deploy();
     });
 
     describe("Deployment", function() {
@@ -40,6 +43,101 @@ describe("NFT", async function() {
             await nft.connect(addr1).mint(1, { value: toWei(price)});
             expect(await nft.balanceOf(addr1.address)).to.equal(1);
 
+            await nft.connect(addr2).mint(2, { value: toWei(price * 2)});
+            expect(await nft.balanceOf(addr2.address)).to.equal(2);
+
+            await expect(nft.connect(addr3).mint(3, { value: toWei(price * 3)})).to.be.revertedWith('Each address may only mint x NFTs!');
+        })
+        it("Should perform owner functions", async function() {
+            let newAmountMintPerAccount = 10
+            let newPrice = 0
+
+            await expect(nft.connect(addr1).setMintEnabled(true)).to.be.revertedWith('Ownable: caller is not the owner');
+            await expect(nft.connect(addr1).setAmountMintPerAccount(newAmountMintPerAccount)).to.be.revertedWith('Ownable: caller is not the owner');
+            await expect(nft.connect(addr1).setPrice(newPrice)).to.be.revertedWith('Ownable: caller is not the owner');
+            await expect(nft.connect(addr1).withdraw()).to.be.revertedWith('Ownable: caller is not the owner');
+            
+            await nft.connect(deployer).setAmountMintPerAccount(newAmountMintPerAccount);
+            expect(await nft.amountMintPerAccount()).to.equal(newAmountMintPerAccount);
+            await nft.connect(deployer).setPrice(newPrice);
+            expect(await nft.getPrice()).to.equal(newPrice);
+        })
+    })
+
+    describe("Planting Deployment", function() {
+        it("Should track the different initialized phases", async function() {
+            expect(parseInt(await planting.phaseDuration(0))).to.equal(0)
+            expect(parseInt(await planting.phaseDuration(1))).to.equal(4 * 3600)
+            expect(parseInt(await planting.phaseDuration(2))).to.equal(18 * 3600)
+            expect(parseInt(await planting.phaseDuration(3))).to.equal(48 * 3600)
+            expect(parseInt(await planting.phaseDuration(4))).to.equal(96 * 3600)
+        })
+
+        it("Should plant through different phases", async function() {
+            let currentPlant = await planting.getPlant(addr1.address)
+            expect(currentPlant.phase).to.equal(0)
+            expect(currentPlant.timestampPhaseStarted).to.equal(0)
+            expect(await planting.currentPhaseFinished(addr1.address)).to.equal(true)
+
+            // 1st plant
+            await planting.connect(addr1).plant()
+            currentPlant = await planting.getPlant(addr1.address)
+            let currentTimestamp = await helpers.time.latest()
+            expect(currentPlant.phase).to.equal(1)
+            expect(currentPlant.timestampPhaseStarted).to.equal(currentTimestamp)
+            expect(await planting.currentPhaseFinished(addr1.address)).to.equal(false)
+            
+            await helpers.time.increase(3 * 3600);
+            expect(await planting.currentPhaseFinished(addr1.address)).to.equal(false)
+            await expect(planting.connect(addr1).plant()).to.be.revertedWith('The current growing phase of your plant is not finished yet!');
+            await helpers.time.increase(1 * 3600);
+            expect(await planting.currentPhaseFinished(addr1.address)).to.equal(true)
+            
+            // 2nd plant
+            await planting.connect(addr1).plant()
+            currentPlant = await planting.getPlant(addr1.address)
+            currentTimestamp = await helpers.time.latest()
+            expect(currentPlant.phase).to.equal(2)
+            expect(currentPlant.timestampPhaseStarted).to.equal(currentTimestamp)
+            expect(await planting.currentPhaseFinished(addr1.address)).to.equal(false)
+            
+            await helpers.time.increase(17 * 3600);
+            expect(await planting.currentPhaseFinished(addr1.address)).to.equal(false)
+            await expect(planting.connect(addr1).plant()).to.be.revertedWith('The current growing phase of your plant is not finished yet!');
+            await helpers.time.increase(1 * 3600);
+            expect(await planting.currentPhaseFinished(addr1.address)).to.equal(true)
+
+            // 3rd plant
+            await planting.connect(addr1).plant()
+            currentPlant = await planting.getPlant(addr1.address)
+            currentTimestamp = await helpers.time.latest()
+            expect(currentPlant.phase).to.equal(3)
+            expect(currentPlant.timestampPhaseStarted).to.equal(currentTimestamp)
+            expect(await planting.currentPhaseFinished(addr1.address)).to.equal(false)
+            
+            await helpers.time.increase(47 * 3600);
+            expect(await planting.currentPhaseFinished(addr1.address)).to.equal(false)
+            await expect(planting.connect(addr1).plant()).to.be.revertedWith('The current growing phase of your plant is not finished yet!');
+            await helpers.time.increase(1 * 3600);
+            expect(await planting.currentPhaseFinished(addr1.address)).to.equal(true)
+
+            // 4th plant
+            await planting.connect(addr1).plant()
+            currentPlant = await planting.getPlant(addr1.address)
+            currentTimestamp = await helpers.time.latest()
+            expect(currentPlant.phase).to.equal(4)
+            expect(currentPlant.timestampPhaseStarted).to.equal(currentTimestamp)
+            expect(await planting.currentPhaseFinished(addr1.address)).to.equal(false)
+            
+            await helpers.time.increase(95 * 3600);
+            expect(await planting.currentPhaseFinished(addr1.address)).to.equal(false)
+            await expect(planting.connect(addr1).plant()).to.be.revertedWith('The current growing phase of your plant is not finished yet!');
+            await helpers.time.increase(1 * 3600);
+            expect(await planting.currentPhaseFinished(addr1.address)).to.equal(true)
+
+            // 5th plant
+            await expect(planting.connect(addr1).plant()).to.be.revertedWith('Your plant already reached maximum growth!');
+            
         })
     })
 })
