604b44b1735826e2b706822d2c898509711ea6d4
Alexis Davidson <alexisdavidson00@gmail.com>
2022-11-28 08:18:23 +0700

    Deploy

--- src/backend/scripts/arguments.js
+++ src/backend/scripts/arguments.js
@@ -1,4 +1,2 @@
 module.exports = [
-  "0xc7edeb3be27541bbaa4ae0c4e4391bda1d3043fdc5db9c1aa6559917bd19d028" // mainnet
-  // "0xbc31375dd73e024b8d6b153e29ec5567ba006493bdac3e2099d4f347c3309d06" // goerli
 ];
---
+++ src/backend/scripts/argumentsPlanting.js
@@ -0,0 +1,3 @@
+module.exports = [
+    "0x51e29a87FeB7DD67146A35f02E037A26e0603f75" // nft address
+];
