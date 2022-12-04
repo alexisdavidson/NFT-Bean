dc738dba7b5b99c54e23b8677fc81a8463922370
Alexis Davidson <alexisdavidson00@gmail.com>
2022-12-04 11:53:27 +0700

    Fix mobile menu row

--- src/frontend/components/ActionMenu.js
+++ src/frontend/components/ActionMenu.js
@@ -7,12 +7,24 @@ const Menu = ({web3Handler, account, closeMenu, toggleMenu, buttonLinkOnClick, s
     return (
         <Row className="actionFrame">
             <Row className="m-0 p-0">
-                <div className="mobileMenuButton" onClick={() => toggleMenu(1)}>MINT A BEAN</div>
-                <div className="mobileMenuButton" onClick={async () => { closeMenu(); if (account == null) await web3Handler(); setMenuFarm(true);}}>FARM</div>
-                <div className="mobileMenuButton" onClick={() => toggleMenu(2)}>HOW TO</div>
-                <div className="mobileMenuButton" onClick={() => buttonLinkOnClick('twitterLink')}>TWITTER</div>
-                <div className="mobileMenuButton" onClick={() => buttonLinkOnClick('discordLink')}>DISCORD</div>
-                <div className="mobileMenuButton" onClick={() => buttonLinkOnClick('aboutusLink')}>ABOUT US</div>
+                <Row className="m-0 p-0">
+                    <div className="mobileMenuButton" onClick={() => toggleMenu(1)}>MINT A BEAN</div>
+                </Row>
+                <Row className="m-0 p-0">
+                    <div className="mobileMenuButton" onClick={async () => { closeMenu(); if (account == null) await web3Handler(); setMenuFarm(true);}}>FARM</div>
+                </Row>
+                <Row className="m-0 p-0">
+                    <div className="mobileMenuButton" onClick={() => toggleMenu(2)}>HOW TO</div>
+                </Row>
+                <Row className="m-0 p-0">
+                    <div className="mobileMenuButton" onClick={() => buttonLinkOnClick('twitterLink')}>TWITTER</div>
+                </Row>
+                <Row className="m-0 p-0">
+                    <div className="mobileMenuButton" onClick={() => buttonLinkOnClick('discordLink')}>DISCORD</div>
+                </Row>
+                <Row className="m-0 p-0">
+                    <div className="mobileMenuButton" onClick={() => buttonLinkOnClick('aboutusLink')}>ABOUT US</div>
+                </Row>
             </Row>
         </Row>
     );
