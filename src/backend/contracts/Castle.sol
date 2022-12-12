277e861fa80f12c9d32fb8ffb83597deee6aed31
Alexis Davidson <alexisdavidson00@gmail.com>
2022-12-12 16:19:54 +0700

    Create 3 smart contracts

---
+++ src/backend/contracts/Castle.sol
@@ -0,0 +1,41 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "erc721a/contracts/ERC721A.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "./NFT_Goose.sol";
+import "./Planting.sol";
+
+contract Castle is Ownable, ReentrancyGuard {
+    ERC20 private token;
+    NFT_Goose private nft;
+    Planting private planting;
+
+    mapping(address => bool) private userAlreadyLooted;
+
+    event LootingSuccessful(address user, uint256 choice);
+
+    constructor(address _plantingAddress, address _tokenAddress, address _nftAddress) {
+        planting = Planting(_plantingAddress);
+        token = ERC20(_tokenAddress);
+        nft = NFT_Goose(_nftAddress);
+    }
+
+    function loot(uint256 _choice) public nonReentrant {
+        require(userAlreadyLooted[msg.sender] == false, "This user already looted the castle.");
+        require(planting.getPlant(msg.sender).phase == 4, "The user is not at the last planting phase.");
+        require(planting.currentPhaseFinished(msg.sender), "The user has not finished its planting phase");
+
+        userAlreadyLooted[msg.sender] = true;
+
+        if (_choice == 0) { // Chose the treasure
+            token.transfer(msg.sender, 1);
+        } else { // Chose the Goose
+            nft.mintForUser(msg.sender);
+        }
+
+        emit LootingSuccessful(msg.sender, _choice);
+    }
+}
